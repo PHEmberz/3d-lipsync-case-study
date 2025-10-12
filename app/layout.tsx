1760bb6e2c5964a299bc56b76310c4ef7c8dabcf
Emberz <kellanq@outlook.com>
2025-10-12 22:58:58 +1100

    finished Hero page title and CTA.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,41 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import Scene from "@/app/components/Scene";
+import React from "react";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "3D LipSync Case Study",
-  description: "3D LipSync Case Study",
+    title: "3D LipSync Case Study",
+    description: "3D LipSync Case Study",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        {/*background*/}
+        <div className="fixed inset-0 -z-10">
+            <Scene/>
+        </div>
+        {/*main*/}
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-export default function Home() {
-  return (
-    <div>
-      <main>
+import Link from "next/link";
 
-      </main>
-    </div>
-  );
+export default function Home() {
+    return (
+        <div id="home" className="flex flex-col min-h-dvh justify-center items-start backdrop-blur-sm pl-[10%]">
+            <h1>Chat in 3D</h1>
+            <h2>Type a message, and your avatar speaks it out loud.</h2>
+            <h2>A whole new way to talk online.</h2>
+            <Link href="/experience">
+                <button>
+                    Start Talking
+                </button>
+            </Link>
+        </div>
+    );
 }
